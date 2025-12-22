f125cab9c015edd745f20c477588bdcfd788250e
Eitan Baron <eitan2007@gmail.com>
2025-12-22 11:40:03 +0000

    שנה את הפונטים והוסף התחברות גם באמצעות חשבון גוגל

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
