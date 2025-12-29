a7ce99157965c9d765a7f56973e25a49c7897b1a
Eitan Baron <eitan2007@gmail.com>
2025-12-29 15:46:24 +0000

    הקטן את רוחב הפנל השמאלי בחצי

--- src/components/album/album-editor.tsx
+++ src/components/album/album-editor.tsx
@@ -289,7 +289,7 @@ export function AlbumEditor({ albumId }: AlbumEditorProps) {
   };
   
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
       <div className="lg:col-span-1 space-y-6">
         <Card>
           <CardHeader>
@@ -380,7 +380,7 @@ export function AlbumEditor({ albumId }: AlbumEditorProps) {
           </>
         )}
       </div>
-      <div className="lg:col-span-2">
+      <div className="lg:col-span-3">
         <AlbumPreview 
           pages={albumPages} 
           config={config} 
